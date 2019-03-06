cf05c2a4254d98e4294604f3f32b5b5c5e734337
Vadim815 <vadiman158@gmail.com>
2019-03-06 11:54:25 +0700

    added .gitignore

---
+++ my_prog.c
@@ -0,0 +1,14 @@
+//#include <graphics.h>
+//#include <iostream>
+//#include <stdlib.h>
+//using namespace std;
+#include <stdio.h>
+
+int main(){
+//	initwindow(640,360);
+//	setcolor(GREEN);
+//	circle(320,180,50);
+//	closegraph();
+	printf("Hello");
+	return 0;
+}
