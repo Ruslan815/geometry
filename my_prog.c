21ada669a52b19ad94913fc0e922a708d7d7c8dc
Vadim815 <vadiman158@gmail.com>
2019-03-06 12:21:53 +0700

    input and output circle

--- my_prog.c
+++ my_prog.c
@@ -1,14 +1,22 @@
-//#include <graphics.h>
-//#include <iostream>
-//#include <stdlib.h>
-//using namespace std;
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-//	initwindow(640,360);
-//	setcolor(GREEN);
-//	circle(320,180,50);
-//	closegraph();
-	printf("Hello");
-	return 0;
+int main()
+{
+    int i, n;
+    float x, y, r;
+    printf("Enter number of figures: ");
+    scanf("%d", &n);
+    float A[n][3];
+    for (i = 1; i <= n; i++) {
+        printf("\n %d.circle= ", i);
+        scanf("%d.circle(%f %f,%f)", &i, &x, &y, &r);
+        A[i][0] = x;
+        A[i][1] = y;
+        A[i][2] = r;
+    }
+    for (i = 1; i <= n; i++) {
+        printf("\n %d.circle(%1.0f %1.0f,%1.2f)", i, A[i][0], A[i][1], A[i][2]);
+    }
+    return 0;
 }
