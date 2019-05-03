48f254db077910fe58d457a86985e60589d6c1c3
Ruslan815 <ruslansaf2000@gmail.com>
2019-05-03 17:59:13 +0800

    Modified files from cpp into c

--- makefile
+++ makefile
@@ -1,18 +1,18 @@
 .PHONY:all clean
-CC=g++
+CC=gcc
 CFLAGS=-Wall -Werror
 SD=~/geometry/src/
 OD=~/geometry/build/
-EXECUTABLE=~/geometry/bin/geometry.exe
+EXECUTABLE=~/geometry/bin/geometry
 all: $(EXECUTABLE)
 	
 $(EXECUTABLE): $(OD)my_prog.o $(OD)perimetr.o $(OD)square.o 
 	$(CC) $(CFLAGS) -o $(EXECUTABLE) $(OD)my_prog.o $(OD)perimetr.o $(OD)square.o -lm
-$(OD)my_prog.o: $(SD)my_prog.cpp
-	$(CC) $(CFLAGS) -c -o $(OD)my_prog.o $(SD)my_prog.cpp -lm
-$(OD)perimetr.o: $(SD)perimetr.cpp
-	$(CC) $(CFLAGS) -c -o $(OD)perimetr.o $(SD)perimetr.cpp -lm
-$(OD)square.o: $(SD)square.cpp
-	$(CC) $(CFLAGS) -c -o $(OD)square.o $(SD)square.cpp -lm
+$(OD)my_prog.o: $(SD)my_prog.c
+	$(CC) $(CFLAGS) -c -o $(OD)my_prog.o $(SD)my_prog.c -lm
+$(OD)perimetr.o: $(SD)perimetr.c
+	$(CC) $(CFLAGS) -c -o $(OD)perimetr.o $(SD)perimetr.c -lm
+$(OD)square.o: $(SD)square.c
+	$(CC) $(CFLAGS) -c -o $(OD)square.o $(SD)square.c -lm
 clean:
 	rm -rf $(EXECUTABLE) $(OD)*.o
---
+++ src/my_prog.c
@@ -0,0 +1,76 @@
+#include "foo.h"
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h>
+const float PI = 3.1415;
+
+typedef struct figure {
+	int x;
+	int y;
+	int r; 	
+};
+
+int main()
+{
+	bool flag=0; 
+    int a, b, c, r, i, j; 
+    float p, s, x, y, z;
+    figure mass[3]; 
+    printf("Enter 3 circles: \n");  
+    for (i=0;i<3;i++) {
+    	printf("\nEnter x,y,radius of circle: "); 
+	    scanf("%f %f %d",&x,&y,&r); 
+	    p = PerimetrC(r, PI);
+	    s = SquareC(r, PI);
+	    mass[i].x=x;
+	    mass[i].y=y;
+	    mass[i].r=r; 
+	    printf("Perimetr: %f Square: %f\n",p,s); 
+	} 
+	
+    for(i=0;i<3;i++) {
+    	for (j=0;j<3;j++) {
+    		if(j==i) {
+    			continue; 
+			} 
+			flag=0; 			
+			r=mass[j].r;
+			x=mass[j].x-r; 
+			z=pow(mass[j].r,2)-pow(x-mass[j].x,2); 
+			z=sqrt(z);
+			y=z+mass[j].y; 			
+			while(x<(mass[j].x+r)) { 
+				if ((pow(x-mass[i].x,2))+(pow(y-mass[i].y,2))<=pow(mass[i].r,2)) {
+					flag=1;  
+					break; 
+				}
+				x+=0.1; 
+				z=pow(mass[j].r,2)-pow(x-mass[j].x,2); 
+				z=sqrt(z);
+				y=z+mass[j].y; 					
+			} 
+			
+			x=mass[j].x-r; 
+			z=pow(mass[j].r,2)-pow(x-mass[j].x,2); 
+			z=sqrt(z); 
+			z*=-1; 
+			y=z+mass[j].y;
+			while(x<(mass[j].x+r)) { 
+				if ((pow(x-mass[i].x,2))+(pow(y-mass[i].y,2))<=pow(mass[i].r,2)) {
+					flag=1;   
+					break; 
+				}
+				x+=0.1; 
+				z=pow(mass[j].r,2)-pow(x-mass[j].x,2); 
+				z=sqrt(z);
+				z*=-1;  
+				y=z+mass[j].y;								
+			}
+			if(flag) {
+				printf("Circle %d crosses circle %d\n",j,i); 
+			}
+		}
+	}
+    
+    return 0;
+}
